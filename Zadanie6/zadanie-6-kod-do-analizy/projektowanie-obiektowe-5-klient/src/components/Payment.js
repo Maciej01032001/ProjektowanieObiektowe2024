6fba8aaa549bb48da859e050ab49e6bb961a4710
Maciej01032001 <baczym@gmail.com>
2024-05-06 19:22:48 +0200

    Reformat: change <input ... to one line in assignment 6

--- Zadanie6/zadanie-6-kod-do-analizy/projektowanie-obiektowe-5-klient/src/components/Payment.js
+++ Zadanie6/zadanie-6-kod-do-analizy/projektowanie-obiektowe-5-klient/src/components/Payment.js
@@ -7,20 +7,12 @@ function Payment({ paymentId, setPaymentId, paymentValue, setPaymentValue, handl
             <h2>Make Payment</h2>
             <label>
                 Enter Payment ID:
-            <input
-                type="text"
-                value={paymentId}
-                onChange={e => setPaymentId(e.target.value)}
-            />
+                <input type="text" value={paymentId} onChange={e => setPaymentId(e.target.value)}/>
             </label>
 
             <label>
                 Enter Payment Value:
-            <input
-                type="text"
-                value={paymentValue}
-                onChange={e => setPaymentValue(e.target.value)}
-            />
+                <input type="text" value={paymentValue} onChange={e => setPaymentValue(e.target.value)}/>
             </label>
 
             <button onClick={handlePayment}>Pay</button>
